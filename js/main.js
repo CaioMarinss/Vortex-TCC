a9e1a70fea7712bbf4969896f1855538e2429f6c
Caio Marins <caio.marins011@gmail.com>
2024-11-06 10:31:49 -0300

    navbar

--- js/main.js
+++ js/main.js
@@ -11,11 +11,3 @@ window.onload = function () {
 
     }, 3000); // 3 segundos para permitir a animação de 1 segundo
 };
-const navbar = document.getElementById('navbar');
-window.addEventListener('scroll', function () {
-    if (window.scrollY > 50) {  // Se a rolagem for maior que 50px
-        navbar.classList.add('scrolled'); // Adiciona a classe para mudar a cor
-    } else {
-        navbar.classList.remove('scrolled'); // Remove a classe quando voltar ao topo
-    }
-});
