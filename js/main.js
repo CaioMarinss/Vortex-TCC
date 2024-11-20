7d7a49843329c41dbc9c60dc9b7eca9fac0db5e0
Caio Marins <marinsroyale@gmail.com>
2024-11-20 18:29:44 -0300

    desktop finalizado (eu acho)

--- js/main.js
+++ js/main.js
@@ -11,25 +11,4 @@ window.onload = function () {
 
     }, 3000); // 3 segundos para permitir a animação de 1 segundo
 };
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-});
 
