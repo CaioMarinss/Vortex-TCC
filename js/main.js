93869db0e4b758f64793de25c6ee17cff2d7c9a3
Caio Marins <marinsroyale@gmail.com>
2024-11-27 00:52:39 -0300

    feat: projeto social v1

--- js/main.js
+++ js/main.js
@@ -12,3 +12,17 @@ window.onload = function () {
     }, 3000); // 3 segundos para permitir a animação de 1 segundo
 };
 
+// swiper js
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
