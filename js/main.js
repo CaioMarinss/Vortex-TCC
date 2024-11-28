f749aef03bd998cde2410bb00e5ef75ccf75462b
Caio Marins <caio.marins011@gmail.com>
2024-11-28 10:05:16 -0300

    add marquee desenho das crianças

--- js/main.js
+++ js/main.js
@@ -16,8 +16,8 @@ let input = document.querySelector('#phone')
 // formatar input de telefone
 function formatPhone(input) {
     let value = input.value.replace(/\D/g, ''); // Remove tudo o que não for número
-    
-    // Formata o número conforme o padrão (XX) XXXXX-XXXX
+
+
     if (value.length <= 2) {
         input.value = `(${value}`;
     } else if (value.length <= 6) {
@@ -30,16 +30,3 @@ function formatPhone(input) {
 
 
 // swiper js
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
