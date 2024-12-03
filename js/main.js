ff26051caa188f648389de07c2c3cad079edb630
Caio Marins <marinsroyale@gmail.com>
2024-12-02 23:25:52 -0300

    arrumando alguns bugs e add botao de voltar ao inicio

--- js/main.js
+++ js/main.js
@@ -43,3 +43,18 @@ const navbar = document.getElementById("navbar");
             }
         }
         window.onscroll = checkIfElementAtTop;
+
+
+        $(document).ready(function () {
+            $(".back-to-top").css("display", "none");
+          $("body").scroll(function() {
+              if($(this).scrollTop() == 0){
+                $(".back-to-top").css("display", "none");
+              } else {
+                $(".back-to-top").css("display", "block");
+              }
+            });
+          $(".back-to-top").click(function() {
+              $("html, body").animate({scrollTop: 0}, 800);
+             });
+          });
